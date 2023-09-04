a32a336227754e47545a9ef4a32cd9e417b418af
addi-anant <addi.anant01@gmail.com>
2023-09-04 12:47:44 +0530

    Deployment Modification - 17

--- Server/index.js
+++ Server/index.js
@@ -52,7 +52,7 @@ mongoose
 app.use("/", require("./routes/index"));
 
 /* Server: */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`The server is up and running on port: ${PORT}`);
 });
