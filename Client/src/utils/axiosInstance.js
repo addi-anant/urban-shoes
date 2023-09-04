91d70c68297901722711dc0acf975c6c45caa1a0
addi-anant <addi.anant01@gmail.com>
2023-09-04 13:10:48 +0530

    Deployment Modification - 18

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://urbanapi.onrender.com/",
+  baseURL: "https://voyance.azurewebsites.net/",
 });
 
 axiosInstance.defaults.withCredentials = true;
